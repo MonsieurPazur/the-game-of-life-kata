c51ac27e0b3d20eee0524b146ea1a91c4481e5f4
Michał Skwarek <michal.skwarek@hotmail.com>
2019-05-16 14:55:45 +0200

    Initial commit, add Game class

---
+++ src/Game.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Handles all main functionalities within Game of Life.
+ */
+
+namespace App;
+
+/**
+ * Class Game
+ * @package App
+ */
+class Game
+{
+
+}
---
+++ tests/GameOfLifeTest.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * Basic test suite for Game of Life interface.
+ */
+
+namespace Test;
+
+use App\Game;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Class GameOfLifeTest
+ * @package Test
+ */
+class GameOfLifeTest extends TestCase
+{
+
+    /**
+     * Tests basic game functionalities.
+     */
+    public function testGame()
+    {
+        $game = new Game();
+        $this->assertTrue(true);
+    }
+}
