68cdf89cb630741af092f89d68d08a23dfc930db
Michał Skwarek <michal.skwarek@hotmail.com>
2019-05-20 08:09:26 +0200

    Move cell logic to separate class

---
+++ src/Cell.php
@@ -0,0 +1,75 @@
+<?php
+
+/**
+ * Simple class for holding internal information for grid cell.
+ */
+
+namespace App;
+
+/**
+ * Class Cell
+ * @package App
+ */
+class Cell
+{
+    /**
+     * @var int Cell that is alive
+     */
+    const ALIVE = 1;
+
+    /**
+     * @var int Cell that is dead
+     */
+    const DEAD = 0;
+
+    /**
+     * @var int $state state of Cell
+     */
+    private $state;
+
+    /**
+     * Cell constructor.
+     *
+     * @param int $state
+     */
+    public function __construct(int $state)
+    {
+        $this->state = $state;
+    }
+
+    /**
+     * Checks whether this cell is alive.
+     *
+     * @return bool true if this cell is alive
+     */
+    public function isAlive(): bool
+    {
+        return $this->state === self::ALIVE;
+    }
+
+    /**
+     * Kills this cell, sets it's internal state to DEAD.
+     */
+    public function kill(): void
+    {
+        $this->state = self::DEAD;
+    }
+
+    /**
+     * Makes this cell alive again, sets it's internal state to ALIVE.
+     */
+    public function ressurect(): void
+    {
+        $this->state = self::ALIVE;
+    }
+
+    /**
+     * Gets current state of a cell. Used for creating fallback to regular array.
+     *
+     * @return int current state (DEAD or ALIVE) of this cell
+     */
+    public function getState(): int
+    {
+        return $this->state;
+    }
+}
--- src/Grid.php
+++ src/Grid.php
@@ -12,43 +12,39 @@ namespace App;
 class Grid
 {
     /**
-     * @var int cell that is alive
-     */
-    const CELL_ALIVE = 1;
-
-    /**
-     * @var int cell that is dead
-     */
-    const CELL_DEAD = 0;
-
-    /**
      * @var array $grid two dimensional matrix
      */
     private $grid;
 
     /**
-     * @var array neighbours matrix
+     * @var array $neighbours neighbours matrix
      */
     private $neighbours;
 
     /**
      * Grid constructor.
-     * @param array $grid
+     *
+     * @param array $initial initial state of game
      */
-    public function __construct(array $grid)
+    public function __construct(array $initial)
     {
-        $this->grid = $grid;
-        $this->initNeighbours();
+        $this->initGrid($initial);
     }
 
     /**
-     * Returns internal grid matrix.
+     * Returns internal grid matrix in a raw form (so just a 2d matrix of 0's and 1's).
      *
      * @return array two dimensional matrix
      */
     public function get(): array
     {
-        return $this->grid;
+        $rawGrid = [];
+        for ($i = 0; $i < count($this->grid); $i++) {
+            for ($j = 0; $j < count($this->grid[0]); $j++) {
+                $rawGrid[$i][$j] = $this->grid[$i][$j]->getState();
+            }
+        }
+        return $rawGrid;
     }
 
     /**
@@ -59,6 +55,22 @@ class Grid
     {
         $this->updateNeighbours();
         $this->updateGrid();
+    }
+
+    /**
+     * Initializes grid with Cells, based on initial values.
+     * Also initializes neighbour matrix with 0's.
+     *
+     * @param array $initial two dimensional array of 0's and 1's
+     */
+    private function initGrid(array $initial): void
+    {
+        for ($i = 0; $i < count($initial); $i++) {
+            for ($j = 0; $j < count($initial[0]); $j++) {
+                $this->neighbours[$i][$j] = 0;
+                $this->grid[$i][$j] = new Cell($initial[$i][$j]);
+            }
+        }
     }
 
     /**
@@ -86,19 +98,6 @@ class Grid
     }
 
     /**
-     * Initializes neighbours matrix.
-     */
-    private function initNeighbours(): void
-    {
-        $this->neighbours = [];
-        for ($i = 0; $i < count($this->grid); $i++) {
-            for ($j = 0; $j < count($this->grid[0]); $j++) {
-                $this->neighbours[$i][$j] = 0;
-            }
-        }
-    }
-
-    /**
      * Gets number of neighbours for a cell of given coordinates.
      *
      * @param int $i y coordinate for grid
@@ -112,7 +111,7 @@ class Grid
         for ($y = $i - 1; $y <= $i + 1; $y++) {
             for ($x = $j - 1; $x <= $j + 1; $x++) {
                 if (isset($this->grid[$y][$x])
-                    && self::CELL_ALIVE === $this->grid[$y][$x]
+                    && $this->grid[$y][$x]->isAlive()
                     && !($x == $j && $y == $i)
                 ) {
                     $neighbours++;
@@ -130,23 +129,23 @@ class Grid
      */
     private function applyRules(int $i, int $j): void
     {
-        if (self::CELL_ALIVE === $this->grid[$i][$j]) {
+        if ($this->grid[$i][$j]->isAlive()) {
             // 1. Any live cell with fewer than two live neighbours dies, as if by underpopulation.
             if ($this->neighbours[$i][$j] < 2) {
-                $this->grid[$i][$j] = self::CELL_DEAD;
+                $this->grid[$i][$j]->kill();
             }
             // 2. Any live cell with two or three live neighbours lives on to the next generation.
             if (2 === $this->neighbours[$i][$j] || 3 === $this->neighbours[$i][$j]) {
-                $this->grid[$i][$j] = self::CELL_ALIVE;
+                $this->grid[$i][$j]->ressurect();
             }
             // 3. Any live cell with more than three live neighbours dies, as if by overpopulation.
             if ($this->neighbours[$i][$j] > 3) {
-                $this->grid[$i][$j] = self::CELL_DEAD;
+                $this->grid[$i][$j]->kill();
             }
         } else {
             // 4. Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
             if (3 === $this->neighbours[$i][$j]) {
-                $this->grid[$i][$j] = self::CELL_ALIVE;
+                $this->grid[$i][$j]->ressurect();
             }
         }
     }
